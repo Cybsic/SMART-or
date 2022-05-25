e06cdf5c3b7995ed17a76fac85331deffa8c9883
simonemancinelli <106096317+simonemancinelli@users.noreply.github.com>
2022-05-25 09:44:39 +0200

    Update SMART-or.py

--- SMART-or.py
+++ SMART-or.py
@@ -3,26 +3,11 @@ from shapely.geometry import Polygon
 import pickle
 
 nv = 100
-def getEs(esb,esa,na):
+def getEs(esb,esa,na): 
     x=(na/nv)*(esa-esb)+esb
     return x
-def line_intersection(line1, line2):
-    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
-    def det(a, b):
-        return a[0] * b[1] - a[1] * b[0]
-
-    div = det(xdiff, ydiff)
-    if div == 0:
-       raise Exception('lines do not intersect')
-
-    d = (det(*line1), det(*line2))
-    x = det(d, xdiff) / div
-    y = det(d, ydiff) / div
-    return x, y
-
-def getInt(esa,eda,esb,edb):
+def getInt(esa,eda,esb,edb): #this compute the intersection between two intervals
     if eda<esb or edb<esa:
         return [0,0]
     elif esa<esb:
@@ -35,7 +20,7 @@ def getInt(esa,eda,esb,edb):
             return [esa,edb]
         else:
             return [esa,eda]
-def getUn(a):
+def getUn(a): #this compute the lenght of the intervals of the union
     b=[]
     nn = a[0][1]-a[0][0]
     b.append(a[0][1])
